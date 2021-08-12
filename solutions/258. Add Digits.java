a49935ff6779eda732c92ba668d1f9435381d876
Siva Prasath S <66523162+Sivaprasathh@users.noreply.github.com>
2021-08-12 22:53:02 +0530

    1 ms

---
+++ solutions/258. Add Digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+       int sum = 0;
+        while(num>0){
+          int rem = num%10;
+            sum =sum+rem;
+            num = num/10;
+            if(sum>9 && num == 0){
+                num=sum;
+                sum=0;
+            }
+        }
+        return sum;
+        }
+    }
+​
+​
