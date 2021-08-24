22655912c89a295d5fce9ac5c0db163d946ae27c
Siva Prasath S <66523162+Sivaprasathh@users.noreply.github.com>
2021-08-24 15:40:34 +0530

    4 ms

---
+++ solutions/179. Largest Number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+          if(nums.length==0)
+            return "";
+        String[] conv=new String[nums.length];
+​
+        for(int i=0;i<nums.length;i++)
+        {
+            conv[i]=Integer.toString(nums[i]);
+        }
+     
+        Arrays.sort(conv,(a,b) -> (b+a).compareTo(a+b));
+        for(String s:conv)
+            System.out.print(s+" ");
+        StringBuilder res=new StringBuilder();
+      
+        if(conv[0].equals("0"))
+            return "0";
+        for(int i=0;i<conv.length;i++)
+        {
+            res.append(conv[i]);
+        }
+        return res.toString();
+    }
+}
